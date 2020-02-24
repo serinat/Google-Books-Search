497bbb0f4ea926ad5f4420dc55e353f9e8655dcf
Serina Tsang <serinat@uw.edu>
2020-02-24 00:26:32 -0800

    updates

--- my-app/src/pages/Saved.js
+++ my-app/src/pages/Saved.js
@@ -1,16 +1,19 @@
 import React from "react";
-//import ViewBtn from "./components/ViewBtn";
-//import DeleteBtn from "./DeleteBtn";
+import Jumbotron from "../components/Jumbotron";
 
 function Saved() {
     return (
         <div>
-            
-                <h1>Saved Books</h1>
-                <h1>Results</h1>
-                
+            <Jumbotron>
+                <h1 class="display-4">(React) Google Books Search</h1>
+                <p class="lead">Search for and save books of interest</p>
+            </Jumbotron>
 
-           
+            <h1>Saved Books</h1>
+            <h1>Results</h1>
+
+
+
         </div>
     )
 }
--- my-app/src/pages/Search.js
+++ my-app/src/pages/Search.js
@@ -1,19 +1,30 @@
 import React from "react";
 import Jumbotron from "../components/Jumbotron";
+import SearchField from "react-search-field";
 
 //import SearchBtn from "./components/SearchBtn";
 //import ViewBtn from "./components/ViewBtn";
 
-function Search() {
+function Search(props) {
     return (
+        <div>
+            <Jumbotron>
+                <h1 class="display-4">(React) Google Books Search</h1>
+                <p class="lead">Search for and save books of interest</p>
+            </Jumbotron>
+            <Jumbotron>
+                <h3>Book Search</h3>
+                <h6>Book title</h6>
+                <SearchField
+                    placeholder="Search..."
+                    onChange={props.handleInputChange} />
+                
 
-        <Jumbotron>
-            <h1 class="display-4">(React) Google Books Search</h1>
-            <p class="lead">Search for and save books of interest</p>
+            </Jumbotron>
 
             
-        </Jumbotron>
-    )
+        </div>
+    );
 }
 
 export default Search;
